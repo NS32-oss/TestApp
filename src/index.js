3e11a86808a550cdaf8662ff3821f04aac169289
Naman <naman03surana@gmail.com>
2024-02-11 22:48:21 +0530

    setup project files p1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = 4000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+app.get('/contact', (req, res) => {
+    res.send('Contact Page');
+
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
